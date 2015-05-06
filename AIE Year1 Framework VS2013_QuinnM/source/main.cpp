100ee5b15a835271bfaece29bbf4402a2e5120e6
QuinnJMosher <Darkstaro59@gmail.com>
2015-05-05 17:40:23 -0700

    more stuff

--- AIE Year1 Framework VS2013_QuinnM/source/main.cpp
+++ AIE Year1 Framework VS2013_QuinnM/source/main.cpp
@@ -1,6 +1,10 @@
 ﻿#include "AIE.h"
 #include <iostream>
 
+#include "GameObject.h"
+#include "GameObjConstructor.h"
+#include "Component.h"
+
 //constant vars
 static int SCREEN_MAX_X = 900, SCREEN_MAX_Y = 600;
 
@@ -11,9 +15,21 @@ int main( int argc, char* argv[] )
     SetBackgroundColour(SColour(0, 0, 0, 255));
 
 
+	//setup tick limiter
+	float tickTime = 0;
+	const float tickInterval = 1.0f / 60;
+
     //Game Loop
     do
 	{
+		tickTime += GetDeltaTime();
+		if (tickTime >= tickInterval) {
+			tickTime -= tickInterval;
+
+			//update
+			//read/act on input
+		}
+		//draw
         ClearScreen();
 
     } while(!FrameworkUpdate());
