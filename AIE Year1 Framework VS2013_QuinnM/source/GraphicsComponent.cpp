07b59ce9ddaaacd2706ef173e054c1652840c603
QuinnJMosher <Darkstaro59@gmail.com>
2015-05-11 17:47:16 -0700

    little thing

--- AIE Year1 Framework VS2013_QuinnM/source/GraphicsComponent.cpp
+++ AIE Year1 Framework VS2013_QuinnM/source/GraphicsComponent.cpp
@@ -1,6 +1,14 @@
 #include "GraphicsComponent.h"
 
-GraphicsComponent::GraphicsComponent(char* immagePath, GameObject& owner);
-GraphicsComponent::GraphicsComponent(unsigned int sprite);
+GraphicsComponent::GraphicsComponent(char* immagePath, GameObject& owner) : Component('G', false) {
+	spriteID = CreateSprite(immagePath, owner.width, owner.height, false);
+}
 
-void GraphicsComponent::Update(GameObject& owner);
+GraphicsComponent::GraphicsComponent(unsigned int sprite) : Component('G', false) {
+	spriteID = sprite;
+}
+
+void GraphicsComponent::Update(GameObject& owner) {
+	MoveSprite(spriteID, owner.x, owner.y);
+	DrawSprite(spriteID);
+}
